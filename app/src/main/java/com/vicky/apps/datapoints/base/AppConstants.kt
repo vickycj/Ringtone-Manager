77b580866a33e99eae52606283d28a52eff74232
vickycj <vygneshwaran@gmail.com>
2020-03-01 22:44:05 +0530

    redesign the layout

--- app/src/main/java/com/vicky/apps/datapoints/base/AppConstants.kt
+++ app/src/main/java/com/vicky/apps/datapoints/base/AppConstants.kt
@@ -5,4 +5,8 @@ object AppConstants {
     const val PERMISION_REQUEST = 100
     const val PERIODIC_INTERVAL = 15L
     var URLFILE = "https://ringtone-app.s3-eu-west-1.amazonaws.com/Ringtone/songringtone.mp3"
+    val SUBSCRIBTION_1_URL = "https://ringtone-app.s3-eu-west-1.amazonaws.com/Ringtone/song1.mp3"
+    val SUBSCRIBTION_2_URL = "https://ringtone-app.s3-eu-west-1.amazonaws.com/Ringtone/song2.mp3"
+    val SUBSCRIBTION_3_URL = "https://ringtone-app.s3-eu-west-1.amazonaws.com/Ringtone/song3.mp3"
+
 }
