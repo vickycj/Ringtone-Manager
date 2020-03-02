4171269fd1d22a00c3ec5417901218abd2d452e8
vickycj <vygneshwaran@gmail.com>
2020-03-02 23:13:06 +0530

    create dashboard activity

---
+++ app/src/main/java/com/vicky/apps/datapoints/ui/view/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.vicky.apps.datapoints.ui.view.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
